89eb455cd6fd53ab4b6b7241dcc57d4db1ce13d6
MatiCava <matiascavallin96@gmail.com>
2025-01-30 21:50:37 -0300

    Arrancando pseudo codigo de 3opt

--- 3_opt.py
+++ 3_opt.py
@@ -82,3 +82,52 @@ def create_list_subsolutions(P, D, S):
                 current_solution = []
     
     return solutions
+
+def generate_3opt_variation(S, change_1, change_2, change_3):
+    # Convertir los segmentos en indices para extraer los valores de S
+    id_1 = S.index(change_1[0]), S.index(change_1[-1]) + 1
+    id_2 = S.index(change_2[0]), S.index(change_2[-1]) + 1
+    id_3 = S.index(change_3[0]), S.index(change_3[-1]) + 1
+
+    # Extraer segmentos segun los indices encontrados
+    part_1 = S[:id_1[0]]  # Desde el inicio hasta antes de change_1
+    part_2 = S[id_1[0]:id_1[1]]  # change_1
+    part_3 = S[id_2[0]:id_2[1]]  # change_2
+    part_4 = S[id_3[0]:id_3[1]]  # change_3
+    part_5 = S[id_3[1]:]  # Desde el final de change_3 hasta el final de S
+
+    # Reorganizar en la nueva permutacion
+    return part_1 + part_2 + part_3 + part_4 + part_5
+
+
+def opt_3(P, D, S, Or, Dest, G, break_percentage):
+    # Inicilizamos el costo original de la solucion con la que arrancamos
+    original_cost = calculate_cost(S, G)
+    # Inicializamos la diferencia con la que vamos a ir checkeando hasta alcanzar el porcentaje de mejora buscado
+    difference = original_cost - original_cost
+    current_percentage = (100 * difference) / original_cost
+    # Buscamos los posibles cambios dentro de la solucion con la que arrancamos
+    possible_changes = create_list_subsolutions(P, D, S, Or, Dest)
+    n = len(possible_changes)
+    
+    while current_percentage < break_percentage:
+        for i, change in enumerate(possible_changes):
+            # Si existen dos posibles cambios hacia delante de donde nos encontramos realizamos una permutacion con ellos
+            # En caso de no encontrar uno o ambos trabajamos con los primeros cambios posibles
+            if i + 1 < n:
+                if i + 2 < n:
+                    new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i + 2])
+                else:
+                    new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i - 1])
+            else:
+                new_solution = generate_3opt_variation(S, change, possible_changes[i - 1], possible_changes[i - 2])
+
+            # Calculamos nuevo costo utilizando la nueva solucion
+            new_cost = calculate_cost(new_solution, G)
+            S = new_solution
+            # Calculamos nueva diferencia entre la solucion original con la que arrancamos y la actual
+            difference = original_cost - new_cost
+            # Calculamos nuevo porcentaje de mejora
+            current_percentage = (100 * difference) / original_cost
+            
+    return new_cost, S
