0a38fc38f7c9c1a33760a60f3886f7460271053c
Mauro <reekremag@gmail.com>
2025-02-10 17:03:02 -0300

    Agrego primera version para grafos de incompatibilidades

--- instance_generator.py
+++ instance_generator.py
@@ -13,6 +13,9 @@ n_values = [5, 10, 15, 20, 25, 30, 35]
 # Letras para identificar instancias
 instance_labels = ['a', 'b', 'c', 'd', 'e']
 
+# Posibles densidades de incompatibilidad
+densities = [0, 0.1, 0.25, 0.5, 0.75]
+
 # Ruta donde guardar las instancias
 base_dir = "Instances"
 os.makedirs(base_dir, exist_ok=True)
@@ -20,11 +23,15 @@ os.makedirs(base_dir, exist_ok=True)
 # Generar las instancias
 for n in n_values:
 
-    # Carpeta correspondiente a las instancias
+    # Carpeta correspondiente a las instancias de tamaño n
     n_dir = os.path.join(base_dir, str(n))  # Carpeta específica para cada n
     os.makedirs(n_dir, exist_ok=True)
     
     for label in instance_labels:
+        # Carpeta correspondiente para esta instancia
+        instance_dir = os.path.join(n_dir, f'prob{n}{label}')
+        os.makedirs(instance_dir, exist_ok=True)
+        
         points = [(random.randint(0, 1000), random.randint(0, 1000)) for _ in range(2 * n + 1)]
         
         # El primer punto es el depósito
@@ -58,18 +65,35 @@ for n in n_values:
                     travel_costs[i][j] = euclidean_distance(all_nodes[i], all_nodes[j])
 
 
-        # Guardar la instancia en un archivo JSON
-        instance_data = {
-            'depot': depot,
-            'final_destination': final_destination,
-            'requests': requests,
-            'travel_costs': travel_costs
-        }
+        # Generamos las 5 variantes de incompatibilidades
+        for density in densities:
 
+            # Inicializar la matriz de incompatibilidades con ceros
+            incompatibility_graph = [[0] * n for _ in range(n)]
 
-        # Guardar las instancias
-        file_name = os.path.join(n_dir, f'prob{n}{label}.json')
-        with open(file_name, 'w') as f:
-            json.dump(instance_data, f, indent=4)
+            # Cantidad de incompatibilidades a generar
+            total_possible_edges = (n * (n - 1)) // 2  # Combinaciones posibles entre items
+            num_incompatibilities = round(density * total_possible_edges)  # Proporción de la densidad
 
-        print(f'Instancia {file_name} creada.')
+            # Generar pares de items incompatibles aleatoriamente
+            possible_pairs = [(i, j) for i in range(n) for j in range(i + 1, n)]
+            selected_pairs = random.sample(possible_pairs, num_incompatibilities)
+
+            for i, j in selected_pairs:
+                incompatibility_graph[i][j] = 1
+                incompatibility_graph[j][i] = 1  # Simetría
+
+            # Guardar la instancia con incompatibilidades
+            instance_data = {
+                'depot': depot,
+                'final_destination': final_destination,
+                'requests': requests,
+                'travel_costs': travel_costs,
+                'incompatibilities': incompatibility_graph
+            }
+
+            file_name = os.path.join(instance_dir, f'density_{density}.json')
+            with open(file_name, 'w') as f:
+                json.dump(instance_data, f, indent=4)
+
+            print(f'Instancia con {density*100}% de incompatibilidades creada: {file_name}')
