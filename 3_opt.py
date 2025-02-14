736cc3259d7f1a034ff1050a58c142bd347a7fa9
MatiCava <matiascavallin96@gmail.com>
2025-02-14 01:48:00 -0300

    Modificacion de como se hace primera solucion y adaptando pseudo codigo de create_list_subsolutions

--- 3_opt.py
+++ 3_opt.py
@@ -63,26 +63,28 @@ def find_disconnected_subgraphs(graph):
 
 # P lista nodos de pickups
 # D lista nodos de delivery
-# S solucion
+# S solucion valida
 def create_list_subsolutions(P, D, S, Or, Dest):
     # Lista de elementos que estan actualmente en el auto
-    shared = []
+    shared = {}
     # Solucion actual
     current_solution = []
     # Lista de listas, donde cada una representa un subgrafo aislado de la solucion
     solutions = []
-
-    for i in S:
-        if i in P:
-            shared.append(i)
-        if i in D:
-            shared.remove(i)
+    for i in range(0, len(S)):
+        node = S[i]
+        node_index = P.index(node) if node in P else D.index(node)
+        if node in P:
+            shared[node_index] = node
+            current_solution.append(node)
+        if node in D:
+            del shared[node_index]
             # Si shared esta vacia, significa que podemos partir la solucion en este punto
             # Lo que sigue en la solucion seria parte de otro subgrafo, ya que no hay items compartidos
-            if not shared and S[i + 1] != Dest:
+            if not shared:
+                current_solution.append(node)
                 solutions.append(current_solution) # Lista de listas
                 current_solution = []
-    
     return solutions
 
 def generate_3opt_variation(S, change_1, change_2, change_3):
@@ -101,55 +103,67 @@ def generate_3opt_variation(S, change_1, change_2, change_3):
     # Reorganizar en la nueva permutacion
     return part_1 + part_4 + part_3 + part_2 + part_5
 
+def calculate_cost(S, input):
+     return 100
 
-def opt_3(P, D, S, Or, Dest, G, break_percentage):
+def opt_3(P, D, S, Or, Dest, input, break_percentage, incompatibilities):
     # Inicilizamos el costo original de la solucion con la que arrancamos
-    original_cost = calculate_cost(S, G)
+    original_cost = calculate_cost(S, input)
     # Inicializamos la diferencia con la que vamos a ir checkeando hasta alcanzar el porcentaje de mejora buscado
     difference = original_cost - original_cost
     current_percentage = (100 * difference) / original_cost
     # Buscamos los posibles cambios dentro de la solucion con la que arrancamos
     possible_changes = create_list_subsolutions(P, D, S, Or, Dest)
-    n = len(possible_changes)
-    
-    while current_percentage < break_percentage:
-        for i, change in enumerate(possible_changes):
-            # Si existen dos posibles cambios hacia delante de donde nos encontramos realizamos una permutacion con ellos
-            # En caso de no encontrar uno o ambos trabajamos con los primeros cambios posibles
-            if i + 1 < n:
-                if i + 2 < n:
-                    new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i + 2])
-                else:
-                    new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i - 1])
-            else:
-                new_solution = generate_3opt_variation(S, change, possible_changes[i - 1], possible_changes[i - 2])
+    print("possible_changes ", possible_changes)
+    # n = len(possible_changes)
 
-            # Calculamos nuevo costo utilizando la nueva solucion
-            new_cost = calculate_cost(new_solution, G)
-            S = new_solution
-            # Calculamos nueva diferencia entre la solucion original con la que arrancamos y la actual
-            difference = original_cost - new_cost
-            # Calculamos nuevo porcentaje de mejora
-            current_percentage = (100 * difference) / original_cost
+    # while current_percentage < break_percentage:
+    #     for i, change in enumerate(possible_changes):
+    #         # Si existen dos posibles cambios hacia delante de donde nos encontramos realizamos una permutacion con ellos
+    #         # En caso de no encontrar uno o ambos trabajamos con los primeros cambios posibles
+    #         if i + 1 < n:
+    #             if i + 2 < n:
+    #                 new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i + 2])
+    #             else:
+    #                 new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i - 1])
+    #         else:
+    #             new_solution = generate_3opt_variation(S, change, possible_changes[i - 1], possible_changes[i - 2])
+
+    #         # Calculamos nuevo costo utilizando la nueva solucion
+    #         new_cost = calculate_cost(new_solution, input)
+    #         S = new_solution
+    #         # Calculamos nueva diferencia entre la solucion original con la que arrancamos y la actual
+    #         difference = original_cost - new_cost
+    #         # Calculamos nuevo porcentaje de mejora
+    #         current_percentage = (100 * difference) / original_cost
             
-    return new_cost, S
+    # return new_cost, S
 
 def generate_initial_solution(input):
     S = []
+    # for i in range(0, len(input["requests"])):
+    #     entry = input["requests"][i]
+    #     if not S:
+    #         S.append([i, entry])
+    #     else:
+    #         check_add = True
+    #         for request_added in S:
+    #             check_add = check_add and input["incompatibilities"][i][request_added[0]] == 0
+    #         if check_add:
+    #             S.append([i, entry])
     for i in range(0, len(input["requests"])):
         entry = input["requests"][i]
-        if not S:
-            S.append([i, entry])
-        else:
-            check_add = True
-            for request_added in S:
-                check_add = check_add and input["incompatibilities"][i][request_added[0]] == 0
-            if check_add:
-                S.append([i, entry])
+        S.append(entry[0])
+        S.append(entry[1])
     return S
 
 def main():
-    route_json = "Instances/5/prob5a/density_0.75.json"
+    route_json = "Instances/5/prob5a/density_0.5.json"
     with open(route_json, "r") as file:
         input = json.load(file)
     initial_S = generate_initial_solution(input)
+    #initial_S = input["requests"]
+    print("initial_S ", initial_S)
+    opt_3(input["pickup_nodes"], input["deliviries_nodes"], initial_S, input["depot"], input["final_destination"], input, 5, input["incompatibilities"])
+
+main()
