11c163c7ffbf31e979c8473e309130a93104c798
MatiCava <matiascavallin96@gmail.com>
2025-02-16 22:26:04 -0300

    Adapte create_list_subsolutions a nodos e intentando hacer andar 3opt

--- 3_opt.py
+++ 3_opt.py
@@ -16,14 +16,14 @@ def create_list_subsolutions(P, D, S, Or, Dest):
     current_solution = []
     # Lista de listas, donde cada una representa un subgrafo aislado de la solucion
     solutions = []
-    for i in range(0, len(S)):
+    # No utilizamos deposito ni destino final
+    for i in range(1, len(S) - 1):
         node = S[i]
-        node_index = P.index(node) if node in P else D.index(node)
         if node in P:
-            shared[node_index] = node
+            shared[node.id] = node
             current_solution.append(node)
         if node in D:
-            del shared[node_index]
+            del shared[node.id]
             # Si shared esta vacia, significa que podemos partir la solucion en este punto
             # Lo que sigue en la solucion seria parte de otro subgrafo, ya que no hay items compartidos
             if not shared:
@@ -33,6 +33,16 @@ def create_list_subsolutions(P, D, S, Or, Dest):
     return solutions
 
 def generate_3opt_variation(S, change_1, change_2, change_3):
+    print("current_S ", S)
+    print("change_1 ", change_1)
+    print("change_1[0] ", change_1[0])
+    print("change_1[-1] ", change_1[-1])
+    print("change_2 ", change_2)
+    print("change_2[0] ", change_2[0])
+    print("change_2[-1] ", change_2[-1])
+    print("change_3 ", change_3) 
+    print("change_3[0] ", change_3[0])
+    print("change_3[-1] ", change_3[-1])
     # Convertir los segmentos en indices para extraer los valores de S
     id_1 = S.index(change_1[0]), S.index(change_1[-1]) + 1
     id_2 = S.index(change_2[0]), S.index(change_2[-1]) + 1
@@ -57,35 +67,44 @@ def calculate_cost(S, travel_costs):
 def opt_3(P, D, S, Or, Dest, travel_costs, break_percentage, incompatibilities):
     # Inicilizamos el costo original de la solucion con la que arrancamos
     original_cost = calculate_cost(S, travel_costs)
+    print("original_cost ", original_cost)
     # Inicializamos la diferencia con la que vamos a ir checkeando hasta alcanzar el porcentaje de mejora buscado
     difference = original_cost - original_cost
     current_percentage = (100 * difference) / original_cost
     # Buscamos los posibles cambios dentro de la solucion con la que arrancamos
     possible_changes = create_list_subsolutions(P, D, S, Or, Dest)
+    print("P ", P)
+    print("D ", D)
+    print("initial_solution ", S)
     print("possible_changes ", possible_changes)
-    # n = len(possible_changes)
+    n = len(possible_changes)
+    while current_percentage < break_percentage:
+        for i in range(0, n):
+            change = possible_changes[i]
+            print("i actual ", i)
+            print("change ", change)
+            # Si existen dos posibles cambios hacia delante de donde nos encontramos realizamos una permutacion con ellos
+            # En caso de no encontrar uno o ambos trabajamos con los primeros cambios posibles
+            if i + 1 < n:
+                if i + 2 < n:
+                    new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i + 2])
+                else:
+                    new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i - 1])
+            else:
+                new_solution = generate_3opt_variation(S, change, possible_changes[i - 1], possible_changes[i - 2])
 
-    # while current_percentage < break_percentage:
-    #     for i, change in enumerate(possible_changes):
-    #         # Si existen dos posibles cambios hacia delante de donde nos encontramos realizamos una permutacion con ellos
-    #         # En caso de no encontrar uno o ambos trabajamos con los primeros cambios posibles
-    #         if i + 1 < n:
-    #             if i + 2 < n:
-    #                 new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i + 2])
-    #             else:
-    #                 new_solution = generate_3opt_variation(S, change, possible_changes[i + 1], possible_changes[i - 1])
-    #         else:
-    #             new_solution = generate_3opt_variation(S, change, possible_changes[i - 1], possible_changes[i - 2])
-
-    #         # Calculamos nuevo costo utilizando la nueva solucion
-    #         new_cost = calculate_cost(new_solution, input)
-    #         S = new_solution
-    #         # Calculamos nueva diferencia entre la solucion original con la que arrancamos y la actual
-    #         difference = original_cost - new_cost
-    #         # Calculamos nuevo porcentaje de mejora
-    #         current_percentage = (100 * difference) / original_cost
+            print("new_solution ", new_solution)
+            # Calculamos nuevo costo utilizando la nueva solucion
+            new_cost = calculate_cost(new_solution, travel_costs)
+            S = new_solution
+            # Calculamos nueva diferencia entre la solucion original con la que arrancamos y la actual
+            difference = original_cost - new_cost
+            # Calculamos nuevo porcentaje de mejora
+            current_percentage = (100 * difference) / original_cost
             
-    # return new_cost, S
+    print("res_solution ", S)
+    print("res_cost ", new_cost)
+    return new_cost, S
 
 def generate_initial_solution(input):
     depot = input["depot"]
@@ -94,7 +113,9 @@ def generate_initial_solution(input):
     final_node = input["final_destination"]
 
     S = [Node(id = depot["id"], x = depot["x"], y = depot["y"], node_type = depot["node_type"])]
-    
+    P = []
+    D = []
+
     for i in range(0, len(pickup_nodes)):
         # pickup y delivery nodes son simetricas
         p = pickup_nodes[i]
@@ -105,17 +126,19 @@ def generate_initial_solution(input):
 
         S.append(node_p)
         S.append(node_d)
+        P.append(node_p)
+        D.append(node_d)
 
     S.append(Node(id = final_node["id"], x = final_node["x"], y = final_node["y"], node_type = final_node["node_type"]))
 
-    return S
+    return S, P, D
 
 
 def main():
     route_json = "Instances/5/prob5a/density_0.5.json"
     with open(route_json, "r") as file:
         input = json.load(file)
-    initial_S = generate_initial_solution(input)
-    opt_3(input["pickup_nodes], input["delivery_nodes], initial_S, input["depot"], input["final_destination"], input["travel_costs"], 5, input["incompatibilities"])
+    initial_S, P, D = generate_initial_solution(input)
+    opt_3(P, D, initial_S, input["depot"], input["final_destination"], input["travel_costs"], 15, input["incompatibilities"])
 
 main()
