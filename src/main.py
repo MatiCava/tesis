9a9fcc3c0302d683b4c819f404d430e464ab963c
unknown <matiascavallin96@gmail.com>
2025-02-21 22:05:27 -0300

    Agrego contabilizacion del tiempo y ajusto funcion de checkeo de soluciones

--- src/main.py
+++ src/main.py
@@ -1,21 +1,17 @@
 import json
 import math
-from utils import generate_initial_solution, is_feasible_solution
+import time
+from utils import generate_initial_solution, generate_routes_json, is_feasible_solution
 from three_opt import opt_3
 import itertools
 
 def main():
-    instance_labels = ['a', 'b', 'c', 'd', 'e']
-    densities = ["0.1", "0.5", "0.25", "0.75", "0"]
-    folders = ["5", "10", "15", "20", "25", "30", "35"]
-    routes_json = []
-    for folder in folders:
-        for label in instance_labels:
-            for density in densities:
-                label_test = "../Instances/"+ folder + "/prob" + folder + label + "/density_" + density + ".json"
-                routes_json.append(label_test)
-                
+
+    routes_json = generate_routes_json()
+    total_execution_time = 0
     for route in routes_json:
+        print(route)
+        start_time = time.time()
         with open(route, "r") as file:
             input = json.load(file)
         break_percentage = 15
@@ -23,7 +19,14 @@ def main():
         len_nodes = len(P) + 2
         max_intentos = math.ceil(break_percentage * (len_nodes * len_nodes) / 100)
         cost, sol = opt_3(P, D, initial_S, input["depot"], input["final_destination"], input["travel_costs"], break_percentage, input["incompatibilities"], max_intentos)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        total_execution_time += execution_time
+        is_correct_sol = is_feasible_solution(sol, input["incompatibilities"])
         print("Costo: ", cost)
         print("Sol: ", sol)
+        print("Es una solucion correcta? ", is_correct_sol)
+        print("Tiempo de ejecucion: ", execution_time)
+    print("Tiempo total de ejecucion: ", total_execution_time)
 
 main()
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,16 @@
 from node import Node
 
+def generate_routes_json():
+    instance_labels = ['a', 'b', 'c', 'd', 'e']
+    densities = ["0.1", "0.5", "0.25", "0.75", "0"]
+    folders = ["5", "10", "15", "20", "25", "30", "35"]
+    routes_json = []
+    for folder in folders:
+        for label in instance_labels:
+            for density in densities:
+                label_test = "../Instances/"+ folder + "/prob" + folder + label + "/density_" + density + ".json"
+                routes_json.append(label_test)
+    return routes_json
 
 def calculate_cost(S, travel_costs):
     total = 0
@@ -9,19 +20,19 @@ def calculate_cost(S, travel_costs):
 
 def is_feasible_solution(S: list[Node], incompatibilities: list[list[int]]):
     active_items = set()
-
-    if S[0].node_type != "depot" or S[-1].node_type != "final":
+    if S[0][0].node_type != "depot" or S[-1][0].node_type != "final":
         return False
 
-    for node in S[1:-1]:
-        if node.node_type == "pickup":
-            if any(incompatibilities[i - 1][node.id - 1] == 1 for i in active_items):
-                return False
-            active_items.add(node.id)
-        else:
-            if node.id not in active_items:
-                return False
-            active_items.remove(node.id)
+    for tuple in S[1:-1]:
+        for node in tuple:
+            if node.node_type == "pickup":
+                if any(incompatibilities[i - 1][node.id - 1] == 1 for i in active_items):
+                    return False
+                active_items.add(node.id)
+            else:
+                if node.id not in active_items:
+                    return False
+                active_items.remove(node.id)
     return True
 
 def generate_initial_solution(input):
